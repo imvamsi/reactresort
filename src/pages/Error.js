a0f230e324073a42b6f6c3e64063d73c15bfeb79
Vamsi <vamsikrishnatamil.selvan@appnovation.com>
2020-01-01 16:41:54 -0500

    added banner on all the pages

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
 export default function Error() {
-    return <Hero/>
+    return (
+    <Hero>
+        <Banner title="404" subtitle="It seems you are lost!!">
+            <Link to = '/' className="btn-primary">
+                Let's take you Home
+            </Link>
+        </Banner>
+    </Hero>
+    );
 }
