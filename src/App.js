8096ee803cd58825fb3b5cfbb37e9086982e15a6
Vamsi <vamsikrishnatamil.selvan@appnovation.com>
2020-01-01 15:59:10 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Home from './pages/Home'
 import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
     <Router>
+      <Navbar/>
       <Switch>
         <Route exact path = '/home' component={Home}/>
         <Route exact path = '/rooms' component={Rooms}/>
---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import {FaAlignRight} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+import logo from '../images/logo.svg'
+export default class Navbar extends Component {
+    state = {
+        navExpanded: false
+    }
+
+     handleToggle = () => {
+        this.setState({navExpanded: !this.state.navExpanded})
+    }
+    render() {
+        return (
+            <nav className="navbar" >
+              <div className="nav-center">
+                <div className="nav-header">
+                    <Link to ='/'>
+                        <img src={logo}/>
+                    </Link>
+                    <button className="nav-btn" type="button" onClick={this.handleToggle}>
+                        <FaAlignRight className="nav-icon"/>
+                    </button>
+                </div>
+                <ul className={this.state.navExpanded ? "nav-links show-nav": "nav-links"}>
+                    <li>
+                        <Link to = '/home'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to = '/rooms'>Rooms</Link>
+                    </li>
+                </ul>
+              </div> 
+            </nav>
+        )
+    }
+}
